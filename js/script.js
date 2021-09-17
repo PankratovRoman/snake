ea2a7fe10f8452d2d10be7fe2bc38819a1771de5
PankratovRoman <80282114+PankratovRoman@users.noreply.github.com>
2021-09-17 08:35:02 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -6,22 +6,33 @@ let cellsInRow = 10;
 let gameAreaSize = cell * cellsInRow;
 let vectorX = 0;
 let vectorY = 0;
+let previousLastSnakeElemCoords = {
+    x: 0,
+    y: 0
+};
 
 canvas.setAttribute("width", gameAreaSize);
 canvas.setAttribute("height", gameAreaSize + 100);
+
+function drawScore() {
+    ctx.fillStyle = "#8FBC8F";
+    ctx.fillRect(0,gameAreaSize, gameAreaSize, 100)
+
+    ctx.fillStyle = "black";
+    ctx.font = "50px Georgia";
+    ctx.fillText(score, cell, gameAreaSize + 50);
+}
 
 function drawGameArea() {
     ctx.fillStyle = "#228B22";
     ctx.fillRect(0,0, gameAreaSize, gameAreaSize)
 
-    ctx.fillStyle = "#8FBC8F";
-    ctx.fillRect(0,gameAreaSize, gameAreaSize, 100)
+    drawScore();
 
     ctx.fillStyle = "#006400";
     for(let i = 0; i < cellsInRow/2; i++) {
         for (let j = 0; j < cellsInRow / 2; j++) {
             ctx.fillRect((cell * j * 2), (cell*2*i), cell, cell);
-            //console.log("X: "+(cell * j * 2)+". Y: "+(cell*2*i))
         }
         for (let j = 0; j < cellsInRow / 2; j++) {
             ctx.fillRect(cell + (cell * j * 2), cell+(cell*2*i), cell, cell);
@@ -45,7 +56,6 @@ let food = {
 
 console.log(food.x + "  " + food.y + "       " +food.x/cellsInRow/cell + "       "+food.y/cellsInRow/cell)
 
-let snake = [];
 
 
 function reset () {
@@ -64,32 +74,35 @@ function reset () {
     };
     score = 0;
     timeScore = defaultTimeScore;
+    previousLastSnakeElemCoords = {
+        x: snake[0].x,
+        y: snake[0].y
+    };
+    drawGameArea();
 }
 
 document.addEventListener("keydown", direction);
-//let dir = "";
 let ignoreTurn = false;
+
 function direction(event) {
-    if(!ignoreTurn) { //защита от быстрого нажатия
+    //защита от быстрого нажатия
+    if(!ignoreTurn) {
         if (event.keyCode == 37 && vectorX == 0) {
             //left
             vectorX = -cell;
             vectorY = 0;
             ignoreTurn = true;
-        }
-        else if(event.keyCode == 38 && vectorY == 0) {
+        } else if(event.keyCode == 38 && vectorY == 0) {
             //up
             vectorX = 0;
             vectorY = -cell;
             ignoreTurn = true;
-        }
-        else if(event.keyCode == 39 && vectorX == 0) {
+        } else if(event.keyCode == 39 && vectorX == 0) {
             //right
             vectorX = cell;
             vectorY = 0;
             ignoreTurn = true;
-        }
-        else if(event.keyCode == 40 && vectorY == 0) {
+        } else if(event.keyCode == 40 && vectorY == 0) {
             //down
             vectorX = 0;
             vectorY = cell;
@@ -112,27 +125,34 @@ function isEatTale(snakeHead, snake) {
     return false;
     }
 
+let snake = [];
 
-function draw() {
-    drawGameArea();
+function drawFoodAndSnake() {
+
     ctx.drawImage(foodImg, food.x, food.y, cell, cell);
 
     for (let i = snake.length - 1; i >= 0; i--) {
         ctx.fillStyle = i == 0 ? "black" : "grey";
         ctx.fillRect(snake[i].x, snake[i].y, cell, cell);
     }
-    ctx.fillStyle = "black";
-    ctx.font = "50px Georgia";
-    ctx.fillText(score, cell, gameAreaSize + 50);
-    setTimeout(draw, 10);
+
+    if (snake[0].x == gameAreaSize/2 && score==0) {
+        ctx.fillStyle = "black";
+    } else if ((previousLastSnakeElemCoords.x / cell) % 2 != 0 && (previousLastSnakeElemCoords.y / cell) % 2 == 0) {
+        ctx.fillStyle = "#006400";
+    } else {
+        ctx.fillStyle = "#228B22";
+    }
+    ctx.fillRect(previousLastSnakeElemCoords.x, previousLastSnakeElemCoords.y, cell, cell)
+    setTimeout(drawFoodAndSnake, 10);
 }
 
 function gameStart() {
     if(!isDead) {
-
-
         let snakeX = snake[0].x;
         let snakeY = snake[0].y;
+        previousLastSnakeElemCoords.x = snake[snake.length - 1].x;
+        previousLastSnakeElemCoords.y = snake[snake.length - 1].y;
 
         if (snakeX < 0 || snakeX >= cell * cellsInRow ||
             snakeY < 0 || snakeY >= cell * cellsInRow) {
@@ -147,13 +167,15 @@ function gameStart() {
 
         if (snakeX == food.x && snakeY == food.y) {
             score++;
+            drawScore();
             timeScore -= (timeScoreDelta > timeScore ? 0 : timeScoreDelta);
             food = {
                 x: Math.floor((Math.random() * cellsInRow)) * cell,
                 y: Math.floor((Math.random() * cellsInRow)) * cell
             };
-        } else if (!isDead)
+        } else if (!isDead) {
             snake.pop();
+        }
 
         let newHead = {
             x: snakeX,
@@ -166,6 +188,8 @@ function gameStart() {
     }
     setTimeout(gameStart, timeScore);
     }
-reset();
-setTimeout(draw, 100);
-gameStart();
+
+
+    reset();
+    setTimeout(drawFoodAndSnake, 100);
+    gameStart();
